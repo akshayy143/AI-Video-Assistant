343408f48ea892fa3f1a0f55d8abaac6027ae9d3
akshay <akshaykumar143143ak@gmail.com>
2026-09-04 13:58:45 +0530

    add code

--- utils/audiopr.py
+++ utils/audiopr.py
@@ -1,3 +1,22 @@
 import yt_dlp
 from pydub import AudioSegment
 import os
+
+DOWNLOAD_DIR = "downloads"
+os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+
+def download_audio(url:str) -> str:
+    output_path = os.path.join(DOWNLOAD_DIR, "%(title)s.%(ext)s")
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': output_path,
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        "quiet": True,
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        
